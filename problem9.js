532a57c684cf2aa4b44aa0fe27f4718d31a2c177
maproctor <adam.proctor@gmail.com>
2017-10-01 02:24:35 -0400

    Using Dickson's Method
    Search the whole space of triplets to find one which satisfies the
    terms.

--- problem9.js
+++ problem9.js
@@ -1,7 +1,16 @@
-// A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 
-// a2 + b2 = c2
-// For example, 32 + 42 = 9 + 16 = 25 = 52.
+for (r=2;r<1000;r++) {
+    let rSquaredOverTwo = (r*r)/2;
+    for (s = 1, t = r*r/2; s <= t; s++,t=r*r/2/s) {
+        if (rSquaredOverTwo%s==0) {  //if it's a factor
 
-// There exists exactly one Pythagorean triplet for which a + b + c = 1000.
-// Find the product abc.
+            //if it adds up to a thousand
+            if (r+s + r+t + r+s+t == 1000) {console.log( (r+s)*(r+t)*(r+s+t) )
+            }
+        }
+    }
+}
+
+
+console.log('test')
+process.exit();
